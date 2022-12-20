2befa4074341060e6c26e36faaaad12d3875bdab
Kristian Hadzhikolev <balccbf@gmail.com>
2022-12-20 01:28:51 +0100

    added routing and testing connection to the backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./layouts/main/main.component";
+import {AuthComponent} from "./layouts/auth/auth.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    loadChildren: () =>
+      import('./pages/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: []
+  },
+  {
+    path: 'dashboard',
+    component: MainComponent,
+    loadChildren: () =>
+      import('./pages/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  constructor(private authService: AuthService) {
+  }
+  async ngOnInit(): Promise<void> {
+    await this.authService.login("lmao", "xd").then(res=>{
+      console.log(res);
+    });
+  }
 }
