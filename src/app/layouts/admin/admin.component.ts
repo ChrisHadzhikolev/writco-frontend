3cd529357746caa84bd46f92877298705bc5ac2c
Kristian Hadzhikolev <balccbf@gmail.com>
2023-01-02 18:07:37 +0100

    admin navigation

--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  constructor(private router: Router, private authService: AuthService) {
+  }
+  categories() {
+    this.router.navigate(["admin/categories"]);
+  }
+
+  comments() {
+    this.router.navigate(["admin/comments"]);
+  }
+
+  userManagement() {
+    this.router.navigate(["admin/usermangement"]);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
